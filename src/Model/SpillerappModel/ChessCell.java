5e6ea2975a0783c2959d9509bdb43dbd01c162a0
Jan Andreas <jan-anan@hotmail.com>
2019-05-07 21:19:47 +0200

    Den morsomme men vanskelige delen :/

--- src/Model/SpillerappModel/ChessCell.java
+++ src/Model/SpillerappModel/ChessCell.java
@@ -2,16 +2,35 @@ package Model.SpillerappModel;
 
 
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class ChessCell extends Pane {
 
-    public ChessCell(){
-        this.setStyle("-fx-background-color: #8211dd;");
+    private String position;
+
+    /**
+     * @param id
+     *      Om id er 1, er panelet hvitt, 0 om grått
+     */
+    public ChessCell(int id, String position){
+        this.position = position;
+        setup(id);
+
+    }
+
+    private void setup(int id){
+        if(id == 1){
+            this.setStyle("-fx-background-color: #d6d9db;");
+        }else{
+            this.setStyle("-fx-background-color: #606060;");
+        }
         this.setPadding(new Insets(30,30,30,30));
         this.setBorder(new Border(new BorderStroke(Color.BLACK,
                 BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         this.setMinSize(80, 80);
+
+        this.getChildren().addAll(new Label(position));
     }
 }
